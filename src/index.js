fff0688d3c3acb91049ec442e43a4e4935c9814c
Sastama <103018192+sastamaa@users.noreply.github.com>
2025-01-05 21:53:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const data = require("./data.json");
+const path = require("path");
+const data = require(path.join(__dirname, "data.json"));
 
 // Middleware
 app.use(cors());
